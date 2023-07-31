7304b40c8b809e78253ae6ba83aa2d62e34653cf
Suman <suman@itapgroup.com>
2023-07-31 21:54:39 +0530

    first commit

---
+++ src/components/GooglePlcasesApi.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from "react";
+import { REACT_APP_GOOGLE_MAPS_KEY } from "../constants/constants";
+
+let autoComplete;
+
+const loadScript = (url, callback) => {
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  if(script.readyState) {
+    script.onreadystatechange = function() {
+      if(script.readyState === 'loaded' || script.readyState === 'complete') {
+        script.onreadystatechange = null;
+        callback()
+      }
+    }
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+const SearchLocationInput = () => {
+  const [query, setQuery] = useState("");
+  const autoCompleteRef= useRef(null);
+
+  const handleScriptLoad = (updateQuery, autoCompleteRef) => {
+    autoComplete = new window.google.maps.places.Autocomplete(
+      autoCompleteRef.current,
+      {
+        types: ['(cities)'],
+        componentRestrictions: { country: 'IN'}
+      }
+    )
+
+    autoComplete.addListener('place_changed', () => {
+      handlePlaceSelect(updateQuery);
+    })
+  }
+
+  const handlePlaceSelect = async updateQuery => {
+    const addressObject = await autoComplete.getPlace();
+
+    const query = addressObject.formatted_address;
+    updateQuery(query);
+    console.log({query});
+
+    const latLng = {
+      lat: addressObject?.geometry?.location?.lat(),
+      lng: addressObject?.geometry?.location?.lng()
+    }
+
+    console.log({latLng})
+  }
+ 
+
+  useEffect(() => {
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_KEY}&libraries=places`,
+       () => handleScriptLoad(setQuery, autoCompleteRef)
+    )
+  }, []);
+
+  return (
+    <div className="search-location-input">
+      <label>Type in your suburb or postcode</label>
+      <input
+        ref={autoCompleteRef}
+        className="form-control"
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Search Places ..."
+        value={query}
+      />
+    </div>
+  );
+};
+
+export default SearchLocationInput;
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const REACT_APP_GOOGLE_MAPS_KEY = "";
